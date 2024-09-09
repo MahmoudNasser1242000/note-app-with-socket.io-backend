06d81d76b5b4329b81fdd99ba2f887ec1692664a
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2024-09-10 01:45:24 +0300

    add note and display all notes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express, { json } from 'express'
 import { Server } from 'socket.io';
 import dbConnection from './database/dbConnection.js';
+import noteModel from './database/models/note.model.js';
 
 const app = express()
 const port = 3000
@@ -18,5 +19,17 @@ io.on('connection', (socket) => {
     console.log('soccket.io connected');
     socket.on("disconnect", () => {
         console.log("app disconnected");
+    })
+
+    socket.on("load", async () => {
+        const notes = await noteModel.find({});
+        socket.emit("display-notes", notes)
+    })
+
+    socket.on("add-note", async (note_data) => {
+        const note = await noteModel.insertMany({title: note_data.title, description: note_data.description});
+
+        const notes = await noteModel.find({});
+        socket.emit("display-notes", notes)
     })
 });
--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/note-app(socjet-io)');
+        await mongoose.connect('mongodb://127.0.0.1/note-app(socjet-io)');
         console.log("connection database");
     } catch (error) {
         console.log("database disconnected", error);
